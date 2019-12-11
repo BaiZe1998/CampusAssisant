f5a0ea833d77119e95c010fc0839588d2f0469bc
YLTFY1998 <1157467179@qq.com>
2019-12-11 14:25:56 +0800

    首次打开时开启定位图层

--- app/src/main/java/com/android/assistant/ui/map/MapFragment.java
+++ app/src/main/java/com/android/assistant/ui/map/MapFragment.java
@@ -1,6 +1,7 @@
 package com.android.assistant.ui.map;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,10 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.android.assistant.R;
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
@@ -20,12 +25,18 @@ import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 public class MapFragment extends Fragment {
 
+    private int flag = 0;               //是否首次定位
     private MapViewModel mapViewModel;
     private MapView mMapView = null;
     private BaiduMap baiduMap;
+    private LocationClient locationClient;
+    private MyLocationListener myLocationListener;
+    private MyLocationData myLocationData;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,13 +48,53 @@ public class MapFragment extends Fragment {
         mMapView = (MapView) root.findViewById(R.id.bmapView);
         // 找到MapView控件，并通过getMap()方法获得BaiduMap类的对象；
         baiduMap = mMapView.getMap();
-        MapStatus.Builder builder = new MapStatus.Builder();
-        builder.zoom(18.0f);
-        baiduMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
-
-
-
+        //定位初始化
+        locationClient = new LocationClient(getActivity());
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        //设置locationClientOption
+        locationClient.setLocOption(option);
+        //注册LocationListener监听器
+        myLocationListener = new MyLocationListener();
+        locationClient.registerLocationListener(myLocationListener);
+        locationClient.start();
+        //开启地图的图层定位
+//        MapStatus.Builder builder = new MapStatus.Builder();
+//        builder.zoom(18.0f);
+//        builder.target(latLng);
+//        baiduMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+        //开启地图定位图层
+        baiduMap.setMyLocationEnabled(true);
         return root;
+    }
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            baiduMap.setMyLocationData(locData);
+            if(flag == 0){//首次打开时获取图层定位
+                flag = 1;
+                //开启地图的图层定位
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.zoom(18.0f);
+                builder.target(latLng);
+                baiduMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+
+            }
+        }
     }
 
     @Override
@@ -63,6 +114,9 @@ public class MapFragment extends Fragment {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
+        locationClient.stop();
+        baiduMap.setMyLocationEnabled(false);
+        mMapView = null;
     }
 
 }
